80efa09d9d54ac735297d75cbcccc3e42b79b884
JohnnyMD <tataru.mda@gmail.com>
2018-09-21 00:10:28 +0200

    Daily Coding #3 added

---
+++ DC__3__.py
@@ -0,0 +1,47 @@
+"""
+This problem was asked by <Google>.
+
+Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+
+For example, given the following Node class
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+The following test should pass:
+
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+assert deserialize(serialize(node)).left.left.val == 'left.left'
+"""
+
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val   = val
+        self.left  = left
+        self.right = right
+
+
+def serialize(root):
+    pass
+
+
+def deserialize(s):
+    pass
+
+
+"""
+    ========================================================================
+    =============================== TESTING ================================
+    ========================================================================
+"""
+
+print("\n'''=====================<Test 1 - begin>===================='''")
+
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+assert deserialize(serialize(node)).left.left.val == 'left.left'
+
+print("'''=====================<Test 1 - end>======================'''\n")
