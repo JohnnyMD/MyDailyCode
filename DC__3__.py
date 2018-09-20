10c65c76519cc32edcecd9a66bad8e309387ca26
JohnnyMD <tataru.mda@gmail.com>
2018-09-21 01:30:03 +0200

    Init solving the Problem #3

--- DC__3__.py
+++ DC__3__.py
@@ -24,10 +24,60 @@ class Node:
         self.left  = left
         self.right = right
 
+# A function to do inorder tree traversal
 
-def serialize(root):
-    pass
 
+def printInorder(root):
+
+    if root:
+
+        # First recur on left child
+        printInorder(root.left)
+
+        # then print the data of node
+        print(root.val),
+
+        # now recur on right child
+        printInorder(root.right)
+
+
+#Z A function to do postorder tree traversal
+def printPostorder(root):
+
+    if root:
+
+        # First recur on left child
+        printPostorder(root.left)
+
+        # the recur on right child
+        printPostorder(root.right)
+
+        # now print the data of node
+        print(root.val),
+
+
+# A function to do postorder tree traversal
+def printPreorder(root):
+
+    if root:
+
+        # First print the data of node
+        print(root.val)
+
+        # Then recur on left child
+        printPreorder(root.left)
+
+        # Finally recur on right child
+        printPreorder(root.right)
+
+
+
+def serialize_helper(root):
+    if root:
+        global ris 
+        ris += root.val
+        serialize_helper(root.left) 
+        serialize_helper(root.right)
 
 def deserialize(s):
     pass
@@ -41,7 +91,12 @@ def deserialize(s):
 
 print("\n'''=====================<Test 1 - begin>===================='''")
 
+ris = ""
+
 node = Node('root', Node('left', Node('left.left')), Node('right'))
-assert deserialize(serialize(node)).left.left.val == 'left.left'
+
+print(ris)
+
+#assert deserialize(serialize(node, "")).left.left.val == 'left.left'
 
 print("'''=====================<Test 1 - end>======================'''\n")
