ccd0cf6202b17b8193c211f33b10ccb74421014c
JohnnyMD <tataru.mda@gmail.com>
2018-09-22 10:24:15 +0200

    New DC problem to solve (#4)

---
+++ DC__4__.py
@@ -0,0 +1,27 @@
+"""
+September 21, 2018
+
+This problem was asked by <Stripe>.
+
+Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+You can modify the input array in-place.
+"""
+
+
+
+
+
+"""
+    ========================================================================
+    =============================== TESTING ================================
+    ========================================================================
+"""
+
+print("\n'''=====================<Test 1 - begin>===================='''")
+
+
+
+print("'''=====================<Test 1 - end>======================'''\n")
