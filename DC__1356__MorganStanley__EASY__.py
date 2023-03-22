5f74d642eb305581861aa739aa400d1d9fc26909
Ion Tataru <tataru.mda+jira@gmail.com>
2023-03-23 00:22:36 +0100

    Init #1356

---
+++ DC__1356__MorganStanley__EASY__.py
@@ -0,0 +1,27 @@
+'''
+    --- --- ---  --- --- ---  --- --- --- ---  --- --- ---  --- --- ---
+    [2023-03-22] Challenge #1356 - [Easy] - asked by Morgan Stanley
+    --- --- ---  --- --- ---  --- --- --- ---  --- --- ---  --- --- ---
+    
+    In Ancient Greece, it was common to write text with the first line going left to right, the second line going right to left, and continuing to go back and forth. This style was called "boustrophedon".
+
+    Given a binary tree, write an algorithm to print the nodes in boustrophedon order.
+
+    For example, given the following tree:
+    
+               1
+           /       \
+          2         3
+         / \       / \
+        4   5     6   7
+    
+    You should return [1, 3, 2, 4, 5, 6, 7].
+'''
+
+
+if __name__ == '__main__':
+    print("\n'''=====================< START TESTS >===================='''")
+
+    print("\n'''=====================<   Test 01   >===================='''")
+    
+    print("\n'''=====================<  END TESTS  >===================='''")
