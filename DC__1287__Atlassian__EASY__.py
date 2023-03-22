468f9bf5fb0e2563e3130251105d210722cdb946
Ion Tataru <tataru.mda+jira@gmail.com>
2023-03-22 23:59:41 +0100

    Init P-1287

---
+++ DC__1287__Atlassian__EASY__.py
@@ -0,0 +1,21 @@
+'''
+    --- --- ---  --- --- ---  --- --- --- ---  --- --- ---  --- --- ---
+    [2022-12-30] Challenge #1287 - [Easy] - asked by Atlassian
+    --- --- ---  --- --- ---  --- --- --- ---  --- --- ---  --- --- ---
+    
+    MegaCorp wants to give bonuses to its employees based on how many lines of codes they have written.
+    They would like to give the smallest positive amount to each worker consistent with the constraint
+    that if a developer has written more lines of code than their neighbor, they should receive more money.
+
+    Given an array representing a line of seats of employees at MegaCorp, determine how much each one should get paid.
+
+    For example, given [10, 40, 200, 1000, 60, 30], you should return [1, 2, 3, 4, 2, 1].
+'''
+
+
+if __name__ == '__main__':
+    print("\n'''=====================< START TESTS >===================='''")
+
+    print("\n'''=====================<   Test 01   >===================='''")
+    
+    print("\n'''=====================<  END TESTS  >===================='''")
