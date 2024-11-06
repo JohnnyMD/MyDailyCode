5e1316abefba8e5edaf88e7e32804e1a2905e99c
Ion Tataru <tataru.mda+jira@gmail.com>
2024-11-06 23:56:02 +0100

    - Init P-1851;

---
+++ DC__1851__PayPal__Medium__.py
@@ -0,0 +1,23 @@
+'''
+    --- --- ---  --- --- ---  --- --- --- ---  --- --- ---  --- --- ---
+    [2024-11-02] Challenge #1851 - [Medium] - asked by PayPal
+    --- --- ---  --- --- ---  --- --- --- ---  --- --- ---  --- --- ---
+
+    Given a string and a number of lines k, print the string in zigzag form. In zigzag, characters are printed out diagonally from top left to bottom right until reaching the kth line, then back up to top right, and so on.
+
+    For example, given the sentence "thisisazigzag" and k = 4, you should print:
+
+    t     a     g
+     h   s z   a
+      i i   i z
+       s     g
+
+'''
+
+
+if __name__ == '__main__':
+    print("\n'''=====================< START TESTS >===================='''")
+
+    print("\n'''=====================<   Test 01   >===================='''")
+    
+    print("\n'''=====================<  END TESTS  >===================='''")
